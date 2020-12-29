5e1002af29dd6c4e8af0f2745b238b1086f06a3a
myotherbrother <darrel.gill@outlook.com>
2020-12-29 09:02:55 -0700

    Initial commit

--- s3_cdk_udemy/s3_cdk_udemy_stack.py
+++ s3_cdk_udemy/s3_cdk_udemy_stack.py
@@ -1,4 +1,7 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_s3 as _s3,
+    core
+)
 
 
 class S3CdkUdemyStack(core.Stack):
@@ -7,3 +10,14 @@ class S3CdkUdemyStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        _s3.Bucket(
+            self,
+            "BucketId",
+            bucket_name="mycdkmobs399",
+            versioned=True,
+            encryption=_s3.BucketEncryption.S3_MANAGED
+
+            
+
+        )
